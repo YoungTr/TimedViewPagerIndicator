ad701ba54d0b51d36628b3f9e07c201415ef7aaa
YoungTr <YoungTr@macdeiMac.local>
2017-03-19 21:35:22 +0800

    initialize attributes

--- app/src/main/java/com/youngtr/timedviewpagerindicator/MainActivity.java
+++ app/src/main/java/com/youngtr/timedviewpagerindicator/MainActivity.java
@@ -3,7 +3,10 @@ package com.youngtr.timedviewpagerindicator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.youngtr.timedviewpagerindicator.example.R;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
